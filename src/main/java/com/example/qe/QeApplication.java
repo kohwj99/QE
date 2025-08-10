38e0ac87ad2628fe88b4323690ee6b5002e07b10
kohwj99 <kohwj99@gmail.com>
2025-08-10 13:30:20 +0800

    first commit

---
+++ src/main/java/com/example/qe/QeApplication.java
@@ -0,0 +1,13 @@
+package com.example.qe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qe/QeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
