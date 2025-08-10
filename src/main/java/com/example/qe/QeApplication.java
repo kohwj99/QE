105928a77e2ff3d4a6c79dc0c0b1fa78a2258ea6
kohwj99 <kohwj99@gmail.com>
2025-08-10 13:50:59 +0800

    working starting project

--- src/main/java/com/example/qe/QeApplication.java
+++ src/main/java/com/example/qe/QeApplication.java
@@ -2,8 +2,14 @@ package com.example.qe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    org.javers.spring.boot.sql.JaversSqlAutoConfiguration.class
+})
 public class QeApplication {
 
 	public static void main(String[] args) {
